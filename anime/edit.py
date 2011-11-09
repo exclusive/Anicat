1cb0d80de307c59a98ff38823e3f077a07454a9d
exc <none@example.org>
2011-11-09 16:48:42 +0400

    edit py: spelling fixes, readonly period message

--- anime/edit.py
+++ anime/edit.py
@@ -25,9 +25,11 @@ def edit(request, itemId=0, modelname='anime', field=None, ajaxSet=True):
     elif modelname not in EDIT_MODELS:
         response['text'] = 'Bad model name passed.'
     elif not request.user.is_active and modelname != 'state': # :cf:
-        response['text'] = 'You cannot doing this.'
+        response['text'] = 'You cannot do this.'
     elif modelname not in EDIBLE_LIST and (datetime.now() - request.user.date_joined).days < 15:
-        response['text'] = 'You cannot doing this now.'
+        response['text'] = 'You cannot do this now. Please wait for {0} days.'.format(
+                (datetime.now() - request.user.date_joided).days
+            )
     else:
         form = None
         obj = None
@@ -157,7 +159,7 @@ def edit(request, itemId=0, modelname='anime', field=None, ajaxSet=True):
 
 def _saveAnimeNames(form, obj):
     if not obj or not obj.id:
-        raise ValueError('%s not exists.' % type(obj).__name__)
+        raise ValueError('%s does not exists.' % type(obj).__name__)
     names = obj.animenames.all()
     cleaned = form.cleaned_data.values()
     newNames = filter(lambda x: x and x not in names, cleaned)
@@ -184,7 +186,7 @@ def _saveAnimeNames(form, obj):
 
 def _saveAnimeLinks(form, obj):
     if not obj or not obj.id:
-        raise ValueError('%s not exists.' % type(obj).__name__)
+        raise ValueError('%s does not exists.' % type(obj).__name__)
     links = list(obj.links.all())
     cleaned = form.cleaned_data
     cleanlinks = [0] * (len(form.cleaned_data)/2)
